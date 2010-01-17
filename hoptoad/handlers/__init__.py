e1da360e8c4f356639b883fcf641395f62d9c1e3
Mahmoud Abdelkader <mabdelkader@gmail.com>
2010-01-16 22:29:27 -0800

    Initial implementation of an extensible hoptoad notification framework

---
+++ hoptoad/handlers/__init__.py
@@ -0,0 +1,32 @@
+"""Implementations of different handlers that communicate with hoptoad in
+various different protocols.
+"""
+import logging
+
+from handlers.threaded import ThreadedNotifier
+
+
+logger = logging.getLogger(__name__)
+
+
+class Notifier(object):
+    """Interface for all various implementations to provide a seamless API
+    to the notification middleware
+
+    """
+    def enqueue(self, payload, timeout):
+        raise NotImplementedError
+
+def get_handler(settings, *args, **kwargs):
+    """Returns an initialized handler object"""
+    all_settings = dir(settings)
+
+    if 'HOPTOAD_HANDLER_NAME' not in all_settings or\
+        settings.HOPTOAD_HANDLER_NAME.lower() == 'threadpool':
+
+        if 'HOPTOAD_THREAD_COUNT' in all_settings:
+            threads = settings.HOPTOAD_THREAD_COUNT
+        else:
+            threads = 4
+
+        return ThreadedNotifier(threads, *args, **kwargs)
